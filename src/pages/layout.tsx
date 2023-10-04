a6796a757611e46deb28f5fcb9d87b743e5dc666
JulienMorel <julien.morel76350@gmail.com>
2023-10-04 09:40:13 +0200

    💄 CSS TO SASS AND LAYOUT

---
+++ src/pages/layout.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <Wrapper>
+      {children}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  height: 100vh;
+`;
+
